8423ce41c275dc5e02a56f3d459af34fed9a36a4
Emmanuel Sunday <135783723+ebubesunday16@users.noreply.github.com>
2026-02-06 13:59:29 +0100

    preparing final push

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import adminRoutes from "./admin.route";
 import applicationRoutes from "./application.routes"; 
 
 const router = Router();
 
-router.use("/admin", adminRoutes);
 router.use("/applications", applicationRoutes); 
 
 export default router;
